cf7929231d0b7eeca0df13a9044d0e412c1de2e4
AloTerrones <36184038+AloTerrones@users.noreply.github.com>
2023-06-07 18:01:54 -0600

    Add files via upload

---
+++ raw_data/ecological_niche_modeling/Ecological_Niche_Model.R
@@ -0,0 +1,480 @@
+# ---- ECOLOGICAL NICHE MODELS 
+
+
+####  Installing packages
+install.packages("dismo") #functions: gbif
+install.packages("dplyr") #functions: select
+install.packages("CoordinateCleaner") #functions: clean_coordinates
+install.packages("devtools") #function install_github
+devtools::install_github("luismurao/nichetoolbox") #function clean_dup
+install.packages("raster") #functions: getData, extract
+install.packages("corrplot") #functions: cor, corrplot (correlation)
+install.packages("rgbif") #functions: occ_download
+install.packages("remotes") #functions buffer_area 
+install.packages("spocc") 
+install.packages("scrubr") 
+install.packages("rgdal") #functions: readOGR
+install.packages("devtools") 
+remotes::install_github("marlonecobos/ellipsenm") #functions buffer_area
+install.packages("stars")
+devtools::install_github("rsbivand/sp@evolution") 
+
+
+
+####  Uploading packages
+library(dismo)
+library(rgbif)
+library(dplyr)
+library(CoordinateCleaner)
+library(raster)
+library(nichetoolbox)
+library(devtools)
+library(factoextra) 
+library(corrplot) 
+library(spocc)
+library(scrubr)
+library(rgdal) 
+library(rgeos)
+library(ellipsenm) 
+library(sf) 
+library(terra) 
+library(stars) 
+library(sp)
+library(ggplot2)
+library(RColorBrewer) #with package "corrplot" (plot correlation) 
+library(ecospat)
+library(rnaturalearth)
+library(rnaturalearthdata)
+library(ggspatial)
+library(dplyr)
+library(rgeos)
+library(rnaturalearthhires)
+library(tiff)
+
+
+
+# Read csv files
+
+complejo <- read.csv("bases_datos/complejo.csv")
+baritula <- read.csv("bases_datos/baritula.csv")
+parva <- read.csv("bases_datos/parva.csv")
+montana <- read.csv("bases_datos/montana.csv")
+
+
+
+# Plotting points
+
+world <- ne_countries(scale = "large", returnclass = "sf")
+mexico <- ne_states(country="mexico", returnclass= "sf")
+
+
+p_complejo <- ggplot(data=world)+
+  geom_sf(color="gray")+
+  geom_sf(data = mexico, fill = NA, color="gray")+
+  coord_sf(xlim=c(-108,-84), ylim=c(10,25))+
+  annotation_scale(location = "bl", width_hint = 0.2, line_width = 0.1) +
+  annotation_north_arrow(location = "bl", which_north = "true", 
+                         pad_x = unit(0.7, "cm"), pad_y = unit(0.7, "cm"),
+                         style = north_arrow_fancy_orienteering) +
+  geom_point(data=complejo,aes(x=longitude, y=latitude),size=1)+
+  xlab("Long")+
+  ylab("Lat")+
+  theme(panel.grid.major = element_line(color = "lightgray", linetype = "3313", size = 0.2), panel.background = element_rect(fill = "aliceblue"))+
+  ggtitle(expression(paste(italic("Diglossa baritula"))))+
+  theme(plot.title = element_text(hjust = 0.5))
+
+p_complejo
+
+
+
+#### Bioclimatic variables
+
+# Downloaded from https://www.worldclim.org/data/worldclim21.html
+# Defining a variable for the 19 bioclimatic layers from worldclim
+BIOS <- stack(list.files(path="./wc2.1_30s_bio/", pattern = "*.tif$",full.names = T)) 
+plot(BIOS[[1]])
+
+BIOS <- crop(BIOS, c(-108,-84,10,25))
+plot(BIOS[[1]])
+points(complejo$longitude, complejo$latitude, col="red", cex=.4, pch=16)
+
+# Create directory
+#dir.create("./BIOS.c")
+
+# Save cropped bioclimatic layers in ascii format
+writeRaster(BIOS, filename=names(BIOS), bylayer=TRUE, format="ascii")
+# If I would like to read this table again:
+# BIOS <- stack(list.files(path="BIOS.c/", pattern = "*.asc",full.names = T))
+
+
+#### Extract values from bioclimatic layers 
+
+complejo_coordinates <- subset(complejo, select=c(longitude, latitude))
+BIOS_VALUES <- raster::extract(BIOS, complejo_coordinates)
+BIOS_VALUES <- cbind.data.frame(complejo, BIOS_VALUES)
+
+#write.csv(BIOS_VALUES_ALL, "bases_datos/bios_values_all.csv", row.names = FALSE)
+
+# Eliminate records with no values 
+BIOS_VALUES <-na.omit(BIOS_VALUES)
+#write.csv(BIOS_VALUES_ALL, "bases_datos/bios_values_all.csv", row.names = FALSE)
+
+
+#### Correlation and PCA analysis
+
+BIOS_VALUES_ALL <- BIOS_VALUES 
+BIOS_VALUES <- subset(BIOS_VALUES_ALL, select=-c(specie, longitude, latitude))
+complejo.cor <-cor(BIOS_VALUES) 
+
+corrplot(complejo.cor, type="lower", order="hclust", tl.col = "black", tl.srt = 20, 
+         tl.cex = 0.7, col=brewer.pal(n=8, name="RdYlBu")) 
+
+# Create directory
+dir.create("./PCA&COR")
+
+#Save correlation analysis in csv format
+write.csv(complejo.cor, file="./PCA&COR/complejo.cor.csv", row.names = T)
+
+# PCA
+complejo.pca <- prcomp(BIOS_VALUES, scale = TRUE)
+
+# install.packages("factoextra") #si
+# Visualization of explained variances (percentages)
+fviz_eig(complejo.pca)
+
+fviz_eig(complejo.pca, choice = c("variance", "eigenvalue"),
+         addlabels = TRUE)
+
+# Visualization of variables (circular plot)
+fviz_pca_var(complejo.pca,
+             col.var = "contrib",
+             gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"), 
+             repel = TRUE) +
+  theme(text = element_text(size = 10),
+        axis.title = element_text(size = 12),
+        axis.text = element_text(size = 12))
+
+# Contributions of variables to PC1
+fviz_contrib(complejo.pca, choice = "var", axes = 1, top = 10)
+
+# Contributions of variables to PC2
+fviz_contrib(complejo.pca, choice = "var", axes = 2, top = 10)
+
+# Contributions of variables to PC3
+fviz_contrib(complejo.pca, choice = "var", axes = 3, top = 10)
+
+# Contributions of variables to PC4
+fviz_contrib(complejo.pca, choice = "var", axes = 4, top = 10)
+
+
+# Summary statistics PCA
+summary(complejo.pca)
+complejo.pca$sdev^2    
+complejo.pca$rotation   
+
+
+# Save PCA results
+write.csv(complejo.pca$sdev^2, file="./PCA&COR/complejo.pca.sdev2.csv", row.names = T)
+write.csv(complejo.pca$rotation, file="./PCA&COR/complejo.pca.rotation.csv", row.names = T)
+
+
+
+#### Creating a shape that represents the area "M" (accesibility area)
+
+# Hypothesis of areas for model calibration following:
+# https://github.com/marlonecobos/ENM_manuals/blob/master/M_hypotheses/Construction_of_simple_Ms.R
+# https://github.com/marlonecobos/ENM_manuals/blob/master/M_hypotheses/M_from_polygon_intersection.R
+
+# getting ecorregions
+download.file("http://assets.worldwildlife.org/publications/15/files/original/official_teow.zip",
+              destfile = file.path(getwd(), "wwf_ecoregions.zip")) 
+unzip(file.path(getwd(), "wwf_ecoregions.zip"), 
+      exdir = file.path(getwd(), "WWF_ecoregions"))
+file.remove(file.path(getwd(), "wwf_ecoregions.zip"))
+ecor <- readOGR("WWF_ecoregions/official", layer = "wwf_terr_ecos")
+
+
+# Areas by buffering records (100 km buffer)
+M_buffer <- buffer_area(complejo, longitude = "longitude", latitude = "latitude", 
+                        buffer_distance = 100)
+
+# Areas using convex hulls (including 75 km buffer)
+M_convex <- convex_area(complejo, longitude = "longitude", latitude = "latitude", 
+                        buffer_distance = 75)
+
+# Areas using concave hulls (including 75 km buffer)
+M_concave <- concave_area(complejo, longitude = "longitude", latitude = "latitude", 
+                          buffer_distance = 75)
+
+# Areas by selecting polygons (including 25 km buffer)
+M_ecorreg <- polygon_selection(complejo, longitude = "longitude", latitude = "latitude",
+                               polygons = ecor, buffer_distance = 25)
+
+# Intersection
+M_intersect <- gIntersection(M_buffer, M_convex)
+M_intersect <- gIntersection(M_intersect, M_ecorreg)
+
+# Visualization
+par(mfrow = c(2, 2), cex = 0.8, mai=c(0.4,0.4,0.4,0.4))
+
+plot(BIOS[[1]]); plot(M_buffer, add=TRUE); 
+points(complejo[, 2:3], col="red", cex=.4, pch=20); 
+legend("topleft", legend = "Buffer", bty = "n")
+
+plot(BIOS[[1]]); plot(M_convex, add=TRUE); 
+points(complejo[, 2:3], col="red", cex=.4, pch=20); 
+legend("topleft", legend = "Convex", bty = "n")
+
+#plot(BIOS[[1]]); plot(M_concave, add=TRUE); 
+#	points(complejo[, 2:3], col="red", cex=.4, pch=20); 
+#	legend("topleft", legend = "Concave", bty = "n")
+
+plot(BIOS[[1]]); plot(M_ecorreg, add=TRUE); 
+points(complejo[, 2:3], col="red", cex=.4, pch=20); 
+legend("topleft", legend = "Ecorregions", bty = "n")
+
+plot(BIOS[[1]]); plot(M_intersect, add=TRUE); 
+points(complejo[, 2:3], col="red", cex=.4, pch=20); 
+legend("topleft", legend = "Intersection", bty = "n")
+
+par(mfrow=c(1,1))
+
+# Saving shapefile
+writeOGR(M_buffer, "complejo.M/", "M_buffer", driver = "ESRI Shapefile") 
+
+
+
+#### Cropping "BIOS" using M_intersect 
+
+BIOS.m <- mask(BIOS, M_buffer)
+plot(BIOS.m[[1]])
+
+# Plotting M, bio1 and points
+
+plot(BIOS[[1]])
+plot(M_buffer, add=TRUE) #se estan graficando dos mapas uno encima del otro
+points(complejo$longitude, complejo$latitude, col="red", cex=.4, pch=16)
+
+
+# Create directory
+#dir.create("./BIOS.m")
+
+# Save cropped bioclimatic layers in ascii format
+writeRaster(BIOS.m, filename=names(BIOS.m), bylayer=TRUE, format="ascii")
+
+# If I would like to read this table again:
+# BIOS.m <- stack(list.files(path="BIOS.m/", pattern = "*.asc",full.names = T))
+
+
+
+#### PAST BIOCLIMATIC LAYERS
+# https://worldclim.org/data/v1.4/paleo1.4.html
+# LIG: Last Interglacial
+# MH: Mid Holocene
+# LGM: Last Glacial Maximum
+# miroc
+# ccsm
+
+# Loading data
+LIG <- stack(list.files(path="./lig_30s_bio/", pattern = "*.bil$",full.names = T)) 
+MH_miroc <- stack(list.files(path="./mrmidbi_30s/", pattern = "*.tif$",full.names = T)) 
+MH_ccsm <- stack(list.files(path="./ccmidbi_30s/", pattern = "*.tif$",full.names = T)) 
+LGM_miroc <- stack(list.files(path="./mrlgmbi_2-5m/", pattern = "*.tif$",full.names = T)) 
+LGM_ccsm <- stack(list.files(path="./cclgmbi_2-5m/", pattern = "*.tif$",full.names = T)) 
+
+plot(LIG[[1]])
+
+# Resampling
+LIG <- raster::resample(LIG, BIOS)
+MH_miroc <- raster::resample(MH_miroc, BIOS)
+MH_ccsm <- raster::resample(MH_ccsm, BIOS)
+LGM_miroc <- raster::resample(LGM_miroc, BIOS)
+LGM_ccsm <- raster::resample(LGM_ccsm, BIOS)
+
+# Croping
+LIG <- crop(LIG, c(-108,-84,10,25))
+plot(LIG[[1]])
+points(complejo$longitude, complejo$latitude, col="red", cex=.4, pch=16)
+
+MH_miroc <- crop(MH_miroc, c(-108,-84,10,25))
+MH_ccsm <- crop(MH_ccsm, c(-108,-84,10,25))
+LGM_miroc <- crop(LGM_miroc, c(-108,-84,10,25))
+LGM_ccsm <- crop(LGM_ccsm, c(-108,-84,10,25))
+
+
+# Create directories
+dir.create("./LIG") 
+dir.create ("./MH_ccsm") 
+dir.create ("./MH_miroc") 
+dir.create ("./LGM_miroc") 
+dir.create ("./LGM_ccsm")
+
+# Save cropped past bioclimatic layers in ascii format
+writeRaster(LIG, filename=names(LIG), bylayer=TRUE, format="ascii")
+writeRaster(MH_miroc, filename=names(MH_miroc), bylayer=TRUE, format="ascii")
+writeRaster(MH_ccsm, filename=names(MH_ccsm), bylayer=TRUE, format="ascii")
+writeRaster(LGM_miroc, filename=names(LGM_miroc), bylayer=TRUE, format="ascii")
+writeRaster(LGM_ccsm, filename=names(LGM_ccsm), bylayer=TRUE, format="ascii")
+
+
+# Checking values in past layers for occurrences
+
+BIOS_VALUES_LIG <- raster::extract(LIG, complejo_coordinates)
+BIOS_VALUES_LIG <- cbind.data.frame(complejo, BIOS_VALUES_LIG)
+write.csv(BIOS_VALUES_LIG, "bases_datos/bios_values_LIG.csv", row.names = FALSE)
+
+BIOS_VALUES_LGM_ccsm <- raster::extract(LGM_ccsm, complejo_coordinates)
+BIOS_VALUES_LGM_ccsm <- cbind.data.frame(complejo, BIOS_VALUES_LGM_ccsm)
+write.csv(BIOS_VALUES_LGM_ccsm, "bases_datos/bios_values_LGM_ccsm.csv", row.names = FALSE)
+
+BIOS_VALUES_LGM_miroc <- raster::extract(LGM_miroc, complejo_coordinates)
+BIOS_VALUES_LGM_miroc <- cbind.data.frame(complejo, BIOS_VALUES_LGM_miroc)
+
+BIOS_VALUES_MH_ccsm <- raster::extract(MH_ccsm, complejo_coordinates)
+BIOS_VALUES_MH_ccsm <- cbind.data.frame(complejo, BIOS_VALUES_MH_ccsm)
+
+BIOS_VALUES_MH_miroc <- raster::extract(MH_miroc, complejo_coordinates)
+BIOS_VALUES_MH_miroc <- cbind.data.frame(complejo, BIOS_VALUES_MH_miroc)
+
+
+# Values from bio4, bio5, bio6, and bio7 are multiplied by 10 in past layers
+
+bio4<-raster("./bios_cut/bio4.asc")
+bio4 <- bio4*10
+writeRaster(bio4, filename="./bios_cut_100/bio4", format="ascii", overwrite=TRUE)
+
+bio5<-raster("./bios_cut/bio5.asc")
+bio5 <- bio5*10
+writeRaster(bio5, filename="./bios_cut_100/bio5", format="ascii", overwrite=TRUE)
+
+bio6<-raster("./bios_cut/bio6.asc")
+bio6 <- bio6*10
+writeRaster(bio6, filename="./bios_cut_100/bio6", format="ascii", overwrite=TRUE)
+
+bio7<-raster("./bios_cut/bio7.asc")
+bio7 <- bio7*10
+writeRaster(bio7, filename="./bios_cut_100/bio7", format="ascii", overwrite=TRUE)
+
+
+
+####  After running analysis in MAXENT 
+####  Uploading median result from maxent
+
+complejo_maxent<-raster("./maxent_resultados/Diglossa_baritula_bios_cut_100_median.asc") 
+plot(complejo_maxent)
+
+complejo_maxent_presente<-raster("./maxent_resultados/Diglossa_baritula_median.asc") 
+plot(complejo_maxent_presente)
+
+
+# Defining spatial extent (extremes) 
+xmin <- extent(complejo_maxent)
+xmax <- extent(complejo_maxent)
+ymin <- extent(complejo_maxent)
+ymax <- extent(complejo_maxent)
+
+# Create a matrix to set the thresholds for the areas
+# which are considered "suitable" or "insuitable". 
+# The first column in the matrix created below represents 
+# the ‘from’ values and the second column represents the 
+# ‘to’ values and the third column sets what those values 
+# should be set to.
+# so here we are setting anything 0-0.1885 to 0 and anything 0.1885-1 as 1 
+# 0.1885 corresponds to Maximum training sensitivity plus 
+# specificity Logistic threshold
+
+m<-c(0, 0.1885, 0, 0.1885, 1, 1)
+rclmat <- matrix(m, ncol=3, byrow=TRUE)
+rclmat
+
+# Created matrix: 
+#        [,1]   [,2] [,3]
+# [1,] 0.0000 0.1885    0
+# [2,] 0.1885 1.0000    1
+
+# Reclasify the values in the output model 
+# as either 0 (species absent) or 1 (species present) 
+# We can plot out the resulting
+# map of predicted presence and absence.
+
+rc<- reclassify(complejo_maxent, rclmat)
+# par(bg = 'gray') 
+plot(rc)
+
+rc_presente<- reclassify(complejo_maxent_presente, rclmat)
+# par(bg = 'gray') 
+plot(rc_presente)
+
+points(complejo$longitude, complejo$latitude, col="red", cex=.4, pch=16)
+
+
+#### Uploading PAST MODELS 
+
+# LGM CCSM
+complejo_LGM_CCSM <- raster("./maxent_resultados/Diglossa_baritula_LGM_CCSM_median.asc")
+plot(complejo_LGM_CCSM)
+rc_LGM_CCSM<- reclassify(complejo_LGM_CCSM, rclmat)
+par(bg = 'gray') 
+plot(rc_LGM_CCSM)
+
+# LGM MIROC
+complejo_LGM_MIROC <- raster("./maxent_resultados/Diglossa_baritula_LGM_MIROC_median.asc")
+plot(complejo_LGM_MIROC)
+rc_LGM_MIROC<- reclassify(complejo_LGM_MIROC, rclmat)
+par(bg = 'gray') 
+plot(rc_LGM_MIROC)
+
+# MH CCSM
+complejo_MH_CCSM <- raster("./maxent_resultados/Diglossa_baritula_MH_CCSM_median.asc")
+plot(complejo_MH_CCSM)
+rc_MH_CCSM<- reclassify(complejo_MH_CCSM, rclmat)
+par(bg = 'gray') 
+plot(rc_MH_CCSM)
+
+# MH MIROC
+complejo_MH_MIROC <- raster("./maxent_resultados/Diglossa_baritula_MH_MIROC_median.asc")
+plot(complejo_MH_MIROC)
+rc_MH_MIROC<- reclassify(complejo_MH_MIROC, rclmat)
+par(bg = 'gray') 
+plot(rc_MH_MIROC)
+
+# LIG
+complejo_LIG <- raster("./maxent_resultados/Diglossa_baritula_LIG_median.asc")
+plot(complejo_LIG)
+rc_LIG<- reclassify(complejo_LIG, rclmat)
+par(bg = 'gray') 
+plot(rc_LIG)
+
+
+#### PLOTS
+
+# background
+par(bg = 'white')
+
+distribution <- readOGR(dsn="diglossa_baritula_distribution/dba071dpgw.shp")
+plot(distribution)
+
+
+# plot using distribution layer
+plot(rc, col=c("lightgray", "lightgray")); plot(distribution, col="#669966", border="#669966", add=TRUE)
+plot(rc, col=c("#CCFFCC", "#CCFFCC")); plot(distribution, col="#669966", border="#669966", add=TRUE)
+plot(rc, col=c("#CCCC99", "#CCCC99")); plot(distribution, col="#669966", border="#669966", add=TRUE)
+plot(rc, col=c("lightgoldenrod", "lightgoldenrod")); plot(distribution, col="#669966", border="#669966", add=TRUE)
+plot(rc, col=c("#CCEEBB", "#CCEEBB")); plot(distribution, col="#669966", border="#669966", add=TRUE)
+
+
+# plot using present (current BIOS) area
+plot(rc, col=c("#CCEEBB", "#CCEEBB"),legend=FALSE); plot(rc_presente, col=c("#CCEEBB", "#669966"), add=TRUE, legend=FALSE)
+
+# plot using M area and occurrence points
+plot(rc, col=c("#CCEEBB", "#CCEEBB")); plot(M_buffer, border="#669966", lwd=2, add=TRUE); points(complejo[, 2:3], col="#669966", cex=.4, pch=17)
+
+# Models 
+plot(rc_LGM_CCSM, col=c("#CCEEBB", "#669966"), legend=FALSE)
+plot(rc_LGM_MIROC, col=c("#CCEEBB", "#669966"), legend=FALSE)
+plot(rc_MH_CCSM, col=c("#CCEEBB", "#669966"), legend=FALSE)
+plot(rc_MH_MIROC, col=c("#CCEEBB", "#669966"), legend=FALSE)
+plot(rc_LIG, col=c("#CCEEBB", "#669966"), legend=FALSE)
+
+
+
---
+++ raw_data/ecological_niche_modeling/Overlap_Niche.R
@@ -0,0 +1,238 @@
+# OVERLAP NICHE TEST Diglossa baritula complex
+
+install.packages("biomod2", type = "binary")
+install.packages("ecospat")
+install.packages("mda")
+install.packages("MODISTools")
+
+
+library(ecospat) # Spatial Ecology Miscellaneous Methods, CRAN v3.1
+#library(mda) # Mixture and Flexible Discriminant Analysis, CRAN v0.5
+library(raster) # Geographic Data Analysis and Modeling, CRAN v3.1-5
+#library(MODISTools) # Interface to the 'MODIS Land Products Subsets' Web Services, CRAN v1.1.1
+library(ade4) #dudi.pca
+library(factoextra)
+
+
+# Following: plantarum.ca/notebooks/ecospat
+
+# Coordinates for subspecies
+baritula <- read.csv("bases_datos/baritula.csv")
+parva <- read.csv("bases_datos/parva.csv")
+montana <- read.csv("bases_datos/montana.csv")
+
+
+# Environmental data for "M" area -accesible area-
+# (or study area)
+
+# -- We need to generate a dataframe with bioclimatic variables for each pixel
+# -- This dataframe corresponds to study area (M area, accessible area)
+# --- Defining a variable for upload bioclimatic layers already cut to "M" size
+# --- This layers come from ENM analysis
+
+varclim <- stack(list.files(path="bios_M_todos/", pattern = "*.asc$",full.names = T)) 
+
+# --- Creating a SpatialPoints DataFrame with values for each pixel in BIO 1 [[1]]
+climpunto <- rasterToPoints(varclim[[1]], fun=NULL, spatial=TRUE)
+
+# --- Extracting values from 19 bioclimatic layers for background
+clim <- extract(varclim, climpunto)
+
+# --- Adding coordinates to previous dataframe
+# --- This will generate a dataframe with coordinates (x,y) and bioclimatic values
+clim <- data.frame(coordinates(climpunto),clim)
+clim <- na.omit(clim) # --- eliminate NA values (0 NAs)
+
+# --- Change order of columns in "clim"
+colnames(clim)
+clim <- clim[, c(1,2,3,14:21,4:13)]
+colnames(clim)
+
+# --- Save dataframe
+#write.csv(clim, file="0_overlap/clim_M.csv", row.names = F) 
+
+
+### Add environmental data to subspecies data: 
+
+# extract values for each database
+
+baritula_coordinates <- subset(baritula, select=c(longitude, latitude))
+parva_coordinates <- subset(parva, select=c(longitude, latitude))
+montana_coordinates <- subset(montana, select=c(longitude, latitude))
+
+
+baritula_v <- raster::extract(varclim, baritula_coordinates)
+baritula_v <- cbind(baritula_coordinates, baritula_v)
+colnames(baritula_v)
+baritula_v <- baritula_v[, c(1,2,3,14:21,4:13)]
+colnames(baritula_v)
+#write.csv(baritula_v, "0_overlap/baritula_v.csv", row.names = FALSE)
+
+parva_v <- raster::extract(varclim, parva_coordinates)
+parva_v <- cbind(parva_coordinates, parva_v)
+colnames(parva_v)
+parva_v <- parva_v[, c(1,2,3,14:21,4:13)]
+colnames(parva_v)
+#write.csv(parva_v, "0_overlap/parva_v.csv", row.names = FALSE)
+
+montana_v <- raster::extract(varclim, montana_coordinates)
+montana_v <- cbind(montana_coordinates, montana_v)
+colnames(montana_v)
+montana_v <- montana_v[, c(1,2,3,14:21,4:13)]
+colnames(montana_v)
+#write.csv(montana_v, "0_overlap/montana_v.csv", row.names = FALSE)
+
+
+#-----
+## pca scores 
+# 19 WorldClim variables
+
+Xvar <- c("wc2.1_30s_bio_1", "wc2.1_30s_bio_2", 
+          "wc2.1_30s_bio_3", "wc2.1_30s_bio_4", 
+          "wc2.1_30s_bio_5", "wc2.1_30s_bio_6", 
+          "wc2.1_30s_bio_7", "wc2.1_30s_bio_8",
+          "wc2.1_30s_bio_9", "wc2.1_30s_bio_10", 
+          "wc2.1_30s_bio_11", "wc2.1_30s_bio_12", 
+          "wc2.1_30s_bio_13", "wc2.1_30s_bio_14",
+          "wc2.1_30s_bio_15", "wc2.1_30s_bio_16",
+          "wc2.1_30s_bio_17", "wc2.1_30s_bio_18",
+          "wc2.1_30s_bio_19")
+nvar <- length(Xvar)
+
+pca.cal <- dudi.pca(clim[, Xvar], center = TRUE,
+                    scale = TRUE, scannf = FALSE, nf = 2)
+summary(pca.cal)
+
+
+# --- Plot variable contribution
+ecospat.plot.contrib(contrib=pca.cal$co, eigen=pca.cal$eig)
+
+fviz_eig(pca.cal)
+
+fviz_eig(pca.cal, choice = c("variance", "eigenvalue"),
+         addlabels = TRUE)
+
+# Visualization of variables (circular plot)
+fviz_pca_var(pca.cal,
+             col.var = "contrib",
+             gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"), 
+             repel = TRUE) +
+    theme(text = element_text(size = 10),
+          axis.title = element_text(size = 12),
+          axis.text = element_text(size = 12))
+
+# Contributions of variables to PC1
+fviz_contrib(pca.cal, choice = "var", axes = 1, top = 10)
+
+# Contributions of variables to PC2
+fviz_contrib(pca.cal, choice = "var", axes = 2, top = 10)
+
+
+
+# subspecies scores
+baritula.scores <- suprow(pca.cal, baritula_v[,Xvar])$li
+montana.scores <- suprow(pca.cal, montana_v[,Xvar])$li
+parva.scores <-suprow(pca.cal, parva_v[,Xvar])$li
+
+scores.clim <- pca.cal$li
+
+
+
+# Ordination data to get actual grids:
+
+baritula.grid <- ecospat.grid.clim.dyn(scores.clim, scores.clim,
+                            baritula.scores, R = 100)
+
+montana.grid <- ecospat.grid.clim.dyn(scores.clim, scores.clim,
+                                       montana.scores, R = 100)
+
+parva.grid <- ecospat.grid.clim.dyn(scores.clim, scores.clim,
+                                       parva.scores, R = 100)
+
+
+par(mfrow = c(1, 1))
+#### --- Plot of environmental space 
+# -- one by one
+ecospat.plot.niche (baritula.grid, title="baritula", name.axis1="PC1", name.axis2="PC2", cor=F)
+ecospat.plot.niche (montana.grid, title="montana", name.axis1="PC1", name.axis2="PC2", cor=F)
+ecospat.plot.niche (parva.grid, title="parva", name.axis1="PC1", name.axis2="PC2", cor=F)
+
+
+# -- overlapped
+ecospat.plot.niche.dyn (baritula.grid, montana.grid, 
+                        title="baritula vs montana", quant=0.1, name.axis1="PC1", name.axis2="PC2", 
+                        col.unf = "darkorange1", col.exp = "deepskyblue3", col.stab = "lightgray",  
+                        transparency = 38)
+
+
+ecospat.plot.niche.dyn (baritula.grid, parva.grid,
+                        title="baritula vs parva", quant=0.1, name.axis1="PC1", name.axis2="PC2", 
+                        col.unf="darkorange1", col.exp = "gold", col.stab ="lightgray", 
+                        transparency = 38)
+
+
+ecospat.plot.niche.dyn (montana.grid, parva.grid, 
+                        title="montana vs parva", quant=0.1, name.axis1="PC1", name.axis2="PC2", 
+                        col.unf="deepskyblue3", col.exp = "gold", col.stab ="lightgray", 
+                        transparency = 38)
+
+
+#### --- Calculate Niche overlap
+# Estos valores son los que se reportan en el paper
+ecospat.niche.overlap (baritula.grid, montana.grid, cor=T) 
+#$D 0.2414861
+#$I 0.3560643
+
+ecospat.niche.overlap (baritula.grid, parva.grid, cor=T) 
+#$D 0.06433742
+#$I 0.2352018
+
+ecospat.niche.overlap (montana.grid, parva.grid, cor=T) 
+#$D 0.06209386
+#$I 0.2383898
+
+
+# -- Calculate niche overlap cor=F
+# if cor=FALSE, the z$uncor objects created by ecospat.grid.clim are used to calculate the overlap. 
+# if cor=TRUE, the z$cor objects are used.
+ecospat.niche.overlap (baritula.grid, montana.grid, cor=F) 
+#$D 0.4962726
+#$I 0.7418024
+
+ecospat.niche.overlap (baritula.grid, parva.grid, cor=F) 
+#$D 0.2347161
+#$I 0.4571215
+
+ecospat.niche.overlap (montana.grid, parva.grid, cor=F) 
+#$D 0.4495401
+#$I 0.6638163
+
+
+
+#### ---- Perform niche equivalence test
+eq.test.bm <-ecospat.niche.equivalency.test(baritula.grid , montana.grid, rep=100) 
+eq.test.bp <-ecospat.niche.equivalency.test(baritula.grid , parva.grid, rep=100) 
+eq.test.mp <-ecospat.niche.equivalency.test(montana.grid , parva.grid, rep=100) 
+
+#### ---- Plot equivalency test
+ecospat.plot.overlap.test(eq.test.bm,"D","Equivalency: baritula vs montana")
+ecospat.plot.overlap.test(eq.test.bp,"D","Equivalency: baritula vs parva")
+ecospat.plot.overlap.test(eq.test.mp,"D","Equivalency: montana vs parva")
+
+
+
+#### ---- Perform niche similarity test
+sim.test.bm <-ecospat.niche.similarity.test(baritula.grid , montana.grid, rep=100) 
+sim.test.bp <-ecospat.niche.similarity.test(baritula.grid , parva.grid, rep=100) 
+sim.test.mp <-ecospat.niche.similarity.test(montana.grid , parva.grid, rep=100) 
+
+sim.test.mb <-ecospat.niche.similarity.test(montana.grid , baritula.grid, rep=100) 
+sim.test.pb <-ecospat.niche.similarity.test(parva.grid , baritula.grid, rep=100) 
+sim.test.pm <-ecospat.niche.similarity.test(parva.grid , montana.grid, rep=100) 
+
+
+#### --- Plot similarity test
+ecospat.plot.overlap.test(sim.test.mb,"D","Similarity: montana vs baritula")
+ecospat.plot.overlap.test(sim.test.pb,"D","Similarity: parva vs baritula")
+ecospat.plot.overlap.test(sim.test.pm,"D","Similarity: parva vs montana")
+
